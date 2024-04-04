65985b60810f6014eebe9a25fad4b4805f1a79a6
Marcin Kawa <lerukcja2002@gmail.com>
2024-04-04 09:57:29 +0200

    simple enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIntex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0,4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIntex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIntex++;
+        target = Waypoints.points[wavepointIntex];
+    }
+}
