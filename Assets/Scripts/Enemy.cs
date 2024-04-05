30e44207664291d84e5d2f5c28aab1e856a8b664
Marcin Kawa <lerukcja2002@gmail.com>
2024-04-05 10:27:08 +0200

    minor fixes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,7 +17,7 @@ public class Enemy : MonoBehaviour
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        if (Vector3.Distance(transform.position, target.position) <= 0,4f)
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
         {
             GetNextWaypoint();
         }
