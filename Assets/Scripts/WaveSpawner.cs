8128e8697d4aa3f788ca25c6baddac48e914a379
Marcin Kawa <lerukcja2002@gmail.com>
2024-04-11 21:38:21 +0200

    new wave after last death

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,41 +1,44 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
-
     public Transform spawnPoint;
-
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
-
     private int waveIndex = 0;
+    private List<GameObject> enemies = new List<GameObject>();
 
     void Update()
     {
-        if (countdown <= 0f)
+        if (enemies.Count == 0 && countdown <= 0f)
         {
             StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
         }
-
         countdown -= Time.deltaTime;
     }
 
     IEnumerator SpawnWave()
     {
         waveIndex++;
-
         for (int i = 0; i < waveIndex; i++)
         {
-            spawnEnemy();
+            SpawnEnemy();
             yield return new WaitForSeconds(0.5f);
         }
     }
 
-    void spawnEnemy()
+    void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation).gameObject;
+        enemies.Add(enemy);
+    }
+
+    public void RemoveEnemy(GameObject enemy)
+    {
+        enemies.Remove(enemy);
     }
 }
