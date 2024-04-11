5e69f0e978040053adc78e57fbeae7d475fba3a0
Marcin Kawa <lerukcja2002@gmail.com>
2024-04-11 15:14:14 +0200

    add basic waves logic

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void spawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
