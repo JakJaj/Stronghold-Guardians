8d377f039815696848c327cf0bfa0c68f86e1917
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-20 22:31:35 +0200

    Add Players profile, money balance check before purchase

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -25,8 +25,18 @@ public class BuildManager : MonoBehaviour
 
     public void BuildTurretOn(Node node)
     {
+        if (PlayerStats.Money < turretToBuild.cost)
+        {
+            Debug.Log("Not enough money to buy that!");
+            return;
+        }
+
+        PlayerStats.Money -= turretToBuild.cost;
+
         GameObject tuerret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = tuerret;
+
+        Debug.Log("Turret build! Money left: " + PlayerStats.Money);
     }
 
     public void SetTurretToBuild(TurretBlueprint turret)
