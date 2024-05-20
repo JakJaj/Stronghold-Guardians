a66b62b8ad1131fb5736807f0c02fe618dd07732
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-20 21:23:04 +0200

    fix Node script

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -11,26 +11,38 @@ public class Node : MonoBehaviour
     private Renderer rend;
     private Color startColor;
 
+    BuildManager buildManager;
+
     void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+        buildManager = BuildManager.instance;
     }
 
     void OnMouseDown()
     {
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            Debug.Log("You need to choose the turret first! - TODO: Display on screen.");
+            return;
+        }
+
         if (turret != null)
         {
             Debug.Log("Can't build there! - TODO: Display on screen.");
             return;
         }
 
-        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        GameObject turretToBuild = buildManager.GetTurretToBuild();
         turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
     }
 
     void OnMouseEnter()
     {
+        if (buildManager.GetTurretToBuild() == null)
+            return;
+
         rend.material.color = hoverColor;
     }
 
