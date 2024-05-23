2ada05d1472d4f57b41df93314f5fbfe9bea7d87
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-23 09:41:41 +0200

    add players lives

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
@@ -7,8 +5,12 @@ public class PlayerStats : MonoBehaviour
     public static int Money;
     public int startMoney = 400;
 
+    public static int Lives;
+    public int startLives = 20;
+
     void Start()
     {
         Money = startMoney;
+        Lives = startLives;
     }
 }
