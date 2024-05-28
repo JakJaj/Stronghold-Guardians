d3a267b5cfa41dded7b3eba50a05873860d3c5c1
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-28 14:35:17 +0200

    minor UI adjustment

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PauseMenu : MonoBehaviour
+{
+    private VisualElement ui;
+    private Button continueButton;
+    private Button retryButton;
+    private Button menuButton;
+
+    void Start()
+    {
+        var uiDocument = FindObjectOfType<UIDocument>();
+        var root = uiDocument.rootVisualElement;
+
+        ui = root.Q<VisualElement>("MenuPanel");
+
+        continueButton = ui.Q<Button>("ContinueButton");
+        retryButton = ui.Q<Button>("RetryButton");
+        menuButton = ui.Q<Button>("MenuButton");
+
+        continueButton.clicked += ContinueGame;
+        retryButton.clicked += Retry;
+        menuButton.clicked += Menu;
+    }
+
+    void ContinueGame()
+    {
+        Time.timeScale = 1; // Kontynuowanie gry
+        ui.style.display = DisplayStyle.None;
+    }
+
+    void Retry()
+    {
+        Time.timeScale = 1; // Ustawienie czasu na normalny
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void Menu()
+    {
+        Time.timeScale = 1; // Ustawienie czasu na normalny
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0; // Zatrzymanie gry
+        ui.style.display = DisplayStyle.Flex;
+    }
+}
