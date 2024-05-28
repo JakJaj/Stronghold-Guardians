52e4cdbf817cc174567f3a743a1c15030e3ea482
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-28 13:51:31 +0200

    Update CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool doMovement = true;
 
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
@@ -23,12 +22,6 @@ public class CameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-            doMovement = !doMovement;
-
-        if (!doMovement)
-            return;
 
         Vector3 pos = transform.position;
 
