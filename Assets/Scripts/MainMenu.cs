3cfb559710fe23f71e40bbacc66f8eff26a31fc1
Marcin Kawa <lerukcja2002@gmail.com>
2024-05-31 10:57:16 +0200

    add all maps to MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,22 +4,50 @@ using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
-    private string levelToLoad = "Wiosna";
+    private string levelSummer = "Lato";
+    private string levelAutumn = "Jesien";
+    private string levelWinter = "Zima";
+    private string levelSpring = "Wiosna";
 
     private void OnEnable()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        var playButton = root.Q<Button>("MainMenuPlayButton");
+        var summerButton = root.Q<Button>("MainMenuSummerButton");
+        var autumnButton = root.Q<Button>("MainMenuAutumnButton");
+        var winterButton = root.Q<Button>("MainMenuWinterButton");
+        var springButton = root.Q<Button>("MainMenuSpringButton");
         var quitButton = root.Q<Button>("MainMenuQuitButton");
 
-        playButton.clicked += Play;
+        summerButton.clicked += summerPlay;
+        autumnButton.clicked += autumnPlay;
+        winterButton.clicked += winterPlay;
+        springButton.clicked += springPlay;
+
         quitButton.clicked += Quit;
     }
 
-    public void Play()
+    public void summerPlay()
     {
-        SceneManager.LoadScene(levelToLoad);
+        SceneManager.LoadScene(levelSummer);
+        Time.timeScale = 1;
+    }
+
+    public void autumnPlay()
+    {
+        SceneManager.LoadScene(levelAutumn);
+        Time.timeScale = 1;
+    }
+
+    public void winterPlay()
+    {
+        SceneManager.LoadScene(levelWinter);
+        Time.timeScale = 1;
+    }
+
+    public void springPlay()
+    {
+        SceneManager.LoadScene(levelSpring);
         Time.timeScale = 1;
     }
 
