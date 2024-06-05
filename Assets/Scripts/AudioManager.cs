52330816dd70a261efab5c2ee363471b69fb809b
Marcin Kawa <lerukcja2002@gmail.com>
2024-06-05 15:59:31 +0200

    modified audio sources

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+    public AudioClip background;
+    public AudioClip damage;
+    public AudioClip ufo_death;
+    public AudioClip canon;
+    public AudioClip catapult;
+    public AudioClip crosbow;
+    public AudioClip place_deffence;
+    public AudioClip tower_destroy;
+    public AudioClip upgrade;
+    public AudioClip buying_staff;
+    public AudioClip game_over;
+    public AudioClip new_wave;
+}
